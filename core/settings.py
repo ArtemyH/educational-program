9b9284f715aade7a8430bc1fae7cf625164e5cf5
Artemy <Prime.95@mail.ru>
2018-01-08 20:22:46 +0300

    feat(settings): add allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '5dfdv@q668@lcx-$m!)@n*+%y)vb8xx8f6+dcd^-2=!w1f_g50'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
